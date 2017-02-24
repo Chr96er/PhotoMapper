2bcb046a810f054ba8b890d231c17f3d0e1f6ebf
Chr96er <christianhomberg@gmail.com>
2017-02-24 08:06:19 +0100

    - add link to github repository

--- PhotoMapper/server.R
+++ PhotoMapper/server.R
@@ -18,7 +18,7 @@ server <- function(input, output) {
   
   
   output$manual <- renderUI({
-    manual("The PhotoMapper application plots uploaded images on a map based on EXIF location and time information. Images without the required tags will be excluded. Note: all files are temporarily uploaded to shinyapps.io servers from which they will be deleted upon closing the application.")
+    manual("The PhotoMapper application plots uploaded images on a map based on EXIF location and time information. Images without the required tags will be excluded. Note: all files are temporarily uploaded to shinyapps.io servers from which they will be deleted upon closing the application. Source code available under https://github.com/Chr96er/PhotoMapper")
   })
   
   output$map <- renderLeaflet({
